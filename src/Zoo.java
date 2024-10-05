80cec96505b356be5eff70544d46e15c1331df5a
EyaDandeni <ayadandani1123@gmail.com>
2024-10-05 13:58:08 +0100

    Prosit 3

--- src/Zoo.java
+++ src/Zoo.java
@@ -4,19 +4,27 @@ public class Zoo {
     Animal[] animals;
     String name;
     String city;
-    int  nbrCages;
+    //int  nbrCages;
+    final int  nbrCages = 25;
 
-    public Zoo(String name, String city,int nbrCages){
+    public Zoo(String name, String city){
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
+        //this.nbrCages = nbrCages;
         this.animals = new Animal[25];
     }
 
     public void displayZoo(){
-        System.out.println( "Zoo= \n"+" name='" + name + '\'' +
+        /*System.out.println( "Zoo= \n"+" name='" + name + '\'' +
                 ", city='" + city + '\'' +
-                ", nbrCages=" + nbrCages );
+                ", nbrCages=" + nbrCages );*/
+
+        for (Animal animal : animals) {
+            if (animal != null) {
+                animal.displayAnimal();
+                System.out.println("- - - - - - - - -");
+            }
+        }
     }
 
     @Override
@@ -27,5 +35,94 @@ public class Zoo {
                 ", city='" + city + '\'' +
                 ", nbrCages=" + nbrCages +
                 '}';
+    }
+
+
+    /* Prosit 3  -- Instruction 10 --  */
+    public Boolean addAnimal(Animal animal) {
+        for (Animal existingAnimal : animals) {
+            if (existingAnimal != null && existingAnimal.name.equals(animal.name)) {
+                System.out.println("L'animal " + animal.name + " existe déjà dans le zoo.");
+                return false;
+            }
+        }
+        for (int i = 0; i < animals.length; i++) {
+            if (animals[i] == null) { // si il y a une place vide dans le tableau
+                animals[i] = animal;
+                return true;
+            }
+        }
+        System.out.println("Le zoo est plein.");
+        return false;
+    }
+
+
+    public int searchAnimal(Animal animal) {
+        if (animal != null) {
+            for (int i = 0; i < animals.length; i++) {
+                if (animals[i] != null && animals[i].name.equals(animal.name)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    public boolean removeAnimal(Animal animal) {
+        // Vérifiez si l'animal est null
+        if (animal == null) {
+            return false;
+        }
+
+        for (int i = 0; i < animals.length; i++) {
+            if (animals[i] != null && animals[i].name.equals(animal.name)) {
+                animals[i] = null;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isZooFull()
+    {
+        boolean full=false;
+        int nbAnimals=0;
+        for (int i=0 ; i<animals.length;i++){
+            if(animals[i]!=null){
+                nbAnimals++;
+            }
+        }
+        if(nbAnimals==nbrCages){
+
+            full=true;
+        }
+        return full;
+    }
+
+
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        int nbrAnimauxZoo1 = 0;
+        int nbrAnimauxZoo2 = 0;
+
+        for (Animal animal : z1.animals) {
+            if (animal != null) {
+                nbrAnimauxZoo1++;
+            }
+        }
+        for (Animal animal : z2.animals) {
+            if (animal != null) {
+                nbrAnimauxZoo2++;
+            }
+        }
+        if (nbrAnimauxZoo1 > nbrAnimauxZoo2) {
+            return z1;
+        } else if (nbrAnimauxZoo2 > nbrAnimauxZoo1) {
+            return z2;
+        } else {
+            return null; // Si ont le mm nbr d'animaux
+        }
+
     }
 }
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -24,7 +24,7 @@ public class ZooManagement {
         lion.isMammal = false;
         lion.name = "Un lion X";
 
-        Zoo myZoo = new Zoo("Safari Park", "Tunis", 20);
+        Zoo myZoo = new Zoo("Safari Park", "Tunis");
 
         /* -----  Prosit 2 - Instruction 7 ------*/
         Animal elephant = new Animal("Elephantida", "Elephant", 2, false);
@@ -38,6 +38,56 @@ public class ZooManagement {
         //System.out.println(myZoo); // Affichage avec hashcode
         System.out.println("\n Affichage avec toString");
         System.out.println(myZoo.toString());
+
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(elephant);
+        System.out.println(" \n -- Affichage des animaux du ZOO --\n ");
+        myZoo.displayZoo();
+
+        /* ----- Prosit 2 - Instruction 11 ------*/
+        int indiceAnimal=myZoo.searchAnimal(elephant);
+        System.out.println("Indice d'animal :"+indiceAnimal);
+
+        /* --- Prosit3 - Instruction12 --- */
+        System.out.println("\n ---- ");
+
+        // Ajout d'un animal déjà présent
+        if (myZoo.addAnimal(lion)) {
+            System.out.println("Lion ajouté au zoo.");
+        } else {
+            System.out.println("Lion est déja présent !! ");
+        }
+
+
+        /*----- Prosit 3 - Instruction 13 -----*/
+        System.out.println("\n ---- ");
+        if (myZoo.removeAnimal(lion)){
+            System.out.println("Lion removed du zoo.");
+            myZoo.displayZoo();
+        }
+        /* -----  Prosit 3 - Instruction 15 ------*/
+        boolean full= myZoo.isZooFull();
+        if (full == true){
+            System.out.println(" \n Le zoo est plein ! ");
+        }
+        else{
+            System.out.println("Le zoo n'est pas encore plein ! ");
+        }
+
+        /* --- Prosit 3 - Instruction 16 --- */
+        Zoo myZoo1=new Zoo("2emeZoo","Bizerte");
+        Animal cat=new Animal("chat","Le chatt",3,true);
+
+        myZoo1.addAnimal(lion);
+        myZoo1.addAnimal(elephant);
+        myZoo1.addAnimal(cat);
+
+        Zoo ZooPlusRempli=Zoo.comparerZoo(myZoo,myZoo1);
+        if (ZooPlusRempli != null) {
+            System.out.println(" \n \n Le zoo avec le plus d'animaux est : " + ZooPlusRempli.name);
+        } else {
+            System.out.println("Les deux zoos ont le même nombre d'animaux.");
+        }
 
     }
 }
